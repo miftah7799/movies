f400ef1b25e125d46378f3235b6fdc66750c3b95
gilingan2 <99949737+gilingan2@users.noreply.github.com>
2025-01-02 14:55:04 +0000

    feat: update navigation site

--- components/site-menu.tsx
+++ components/site-menu.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import Link from "next/link"
-import { NavItem, navigation, siteConfig } from "@/config"
+import { NavItem, navigation } from "@/config"
 import { useDialog } from "@/hooks"
-import { Bug, MenuIcon } from "lucide-react"
+import { MenuIcon } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import {
@@ -16,13 +16,11 @@ import { Button, buttonVariants } from "@/components/ui/button"
 import {
   Drawer,
   DrawerContent,
-  DrawerFooter,
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer"
 import { Separator } from "@/components/ui/separator"
-import { Icons } from "@/components/icons"
 
 export const SiteMenu = () => {
   const [open, setOpen] = useDialog()
@@ -51,30 +49,6 @@ export const SiteMenu = () => {
         </Accordion>
 
         <Separator className="mt-4" />
-
-        <DrawerFooter>
-          <nav className="flex gap-2">
-            <Link
-              href={siteConfig.links.github}
-              className={cn(buttonVariants({ variant: "outline" }), "flex-1")}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <Icons.Github className="mr-2 size-4 fill-current" />
-              <span>Source code</span>
-            </Link>
-
-            <a
-              href={siteConfig.links.github + "/issues"}
-              className={cn(buttonVariants({ variant: "outline" }), "flex-1")}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Bug className="mr-2 inline size-4 align-middle" />
-              Submit a bug
-            </a>
-          </nav>
-        </DrawerFooter>
       </DrawerContent>
     </Drawer>
   )
