3294d3f0e40b12369123794c58a4fd3f2d8a659d
gilingan2 <99949737+gilingan2@users.noreply.github.com>
2025-01-12 13:40:29 +0000

    Feat add Dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
+  compress: false,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: process.env.CODESPACES
+          ? [
+              {
+                key: "X-Forwarded-Host",
+                value: `${process.env.CODESPACE_NAME}-3000.github.dev`,
+              },
+              {
+                key: "Access-Control-Allow-Origin",
+                value: `https://${process.env.CODESPACE_NAME}-3000.github.dev`,
+              },
+            ]
+          : [],
+      },
+    ]
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:3000",
+        `https://${process.env.CODESPACE_NAME}-3000.github.dev`,
+      ],
+    },
+  },
 }
 
 export default nextConfig
